6a14e61a9128630d73cd1b18e5020c22d51f5c1a
shonm520 <342698145@qq.com>
2020-09-28 21:33:09 +0800

    Update efficiency_sort.lua
    add heap sort

--- efficiency_sort.lua
+++ efficiency_sort.lua
@@ -1,10 +1,11 @@
 --[[
   高效排序的实现
-  data:20190702
+  20190702 快速排序
+  20200928 堆排序
 ]]
 
 math.randomseed(os.time())	 
-local function quick_sort(arr)
+local function quick_sort(arr)           --快速排序
     local partion = function(arr, start, tail)
         if start >= tail then
             return -1
@@ -34,4 +35,34 @@ local function quick_sort(arr)
     end
     
     sort(arr, 1, #arr)
+end
+
+
+
+
+local function heap_sort(arr_input)               --堆排序
+
+    local function adjust(arr, len, index)        --len的作用仅仅只是限制元素比较的范围 index实际是三元组的顶元素
+        local left = 2 * index                    --三元组的左右节点
+        local right = left + 1
+
+        local max_index = index                   --假设目前最大元素是顶元素
+        if left < len and arr[left] > arr[max_index] then max_index = left end
+        if right < len and arr[right] > arr[max_index] then max_index = right end
+
+        if max_index ~= index then                --如果三元组的最大元素不是他的顶元素 则把目前的顶元素和最大元素交换
+            arr[max_index], arr[index] = arr[index], arr[max_index]
+            adjust(arr, len, max_index)           --交换之后可能会影响被交换元素为顶组成的三元组的规则 所以再次调整那个三元组 递归下去
+        end
+    end
+
+    local size = #arr_input
+    for i = size // 2, 1,  -1 do                  --从最后一个非叶子节点开始 即最后一组三元组(最后一组可能只有2个元素)
+        adjust(arr_input, size, i)
+    end
+
+    for i = size, 1, -1 do
+        arr_input[1], arr_input[i] = arr_input[i], arr_input[1]       --把第i个(从最后一个元素开始依次，该元素就是目前未排序中最大的元素)元素同根元素交换
+        adjust(arr_input, i, 1)                     --这时只需要从根步调整三元组，因为他会递归完成的
+    end
 end
