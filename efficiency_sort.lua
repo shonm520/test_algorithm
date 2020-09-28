f1b47fea7e7b4450d07c9e5443c2e985ffb39c04
shonm520 <342698145@qq.com>
2020-09-28 21:36:18 +0800

    Update efficiency_sort.lua
    add heap sort

--- efficiency_sort.lua
+++ efficiency_sort.lua
@@ -39,20 +39,19 @@ end
 
 
 
-
 local function heap_sort(arr_input)               --堆排序
 
-    local function adjust(arr, len, index)        --len的作用仅仅只是限制元素比较的范围 index实际是三元组的顶元素
+    local function adjust(arr, len, index)        --len的作用仅仅只是限制元素比较的范围 index实际是三元组的父节点
         local left = 2 * index                    --三元组的左右节点
         local right = left + 1
 
-        local max_index = index                   --假设目前最大元素是顶元素
+        local max_index = index                   --假设目前最大元素是父节点
         if left < len and arr[left] > arr[max_index] then max_index = left end
         if right < len and arr[right] > arr[max_index] then max_index = right end
 
-        if max_index ~= index then                --如果三元组的最大元素不是他的顶元素 则把目前的顶元素和最大元素交换
+        if max_index ~= index then                --如果三元组的最大元素不是他的父节点 则把目前的父节点和最大元素交换
             arr[max_index], arr[index] = arr[index], arr[max_index]
-            adjust(arr, len, max_index)           --交换之后可能会影响被交换元素为顶组成的三元组的规则 所以再次调整那个三元组 递归下去
+            adjust(arr, len, max_index)           --交换之后可能会影响被交换元素为父节点组成的三元组的规则 所以再次调整那个三元组 递归下去
         end
     end
 
@@ -66,3 +65,4 @@ local function heap_sort(arr_input)               --堆排序
         adjust(arr_input, i, 1)                     --这时只需要从根步调整三元组，因为他会递归完成的
     end
 end
+
